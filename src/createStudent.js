585d4f786ee1570cbb7195ea2f92fe2375ca9269
Claudio Melgaco <claudio19a@hotmail.com>
2022-06-27 15:04:12 -0300

    req 5

--- src/createStudent.js
+++ src/createStudent.js
@@ -15,6 +15,12 @@
     estudante.feedback() // Retorna: 'Eita pessoa boa!'
 */
 
-const createStudent = () => {};
+const createStudent = (name) => {
+  const studentName = {
+    name: name,
+    feedback: () => 'Eita pessoa boa!'
+  }
+  return studentName;
+};
 
 module.exports = createStudent;
